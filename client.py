a175fc5fb69a417426326b343ea4644eca9e5f59
bot-boi <bananajones532@gmail.com>
2019-09-07 23:09:08 -0400

    add fns to get client window, frame, and canvas.  start Client class

--- client.py
+++ client.py
@@ -1,8 +1,59 @@
 # client state evaluation and window interface
 from enum import Enum
+from ewmh import EWMH
+ewmh = EWMH()
 
-class CS(Enum):
+# get parent of target client window that is also child
+# of root window (?).  i dont entirely understand how this works
+# but this is how you get a client object with a valid x,y
+# offset relative to desktop dimensions
+
+def get_window(title):
+    title = title.encode() # needs to be byte
+    clients = ewmh.getClientList()
+    for client in clients:
+        if title in ewmh.getWmName(client):
+            return client
+
+def get_frame(client):
+    frame = client
+    while frame.query_tree().parent != ewmh.root:
+        frame = frame.query_tree().parent
+        print(ewmh.getWmName(frame))
+        print(frame.get_geometry())
+    return frame
+
+def get_canvas_recursive(client): # unfinished, cant seem to figure this out reeeee
+    canvas_name = "sun-awt-X11-XCanvasPeer".encode()
+    result = None
+    for child in client.query_tree().children:
+        if ewmh.getWmName(child) == canvas_name:
+            result = child
+            break
+        get_canvas_recursive(child)
+
+def get_canvas(client): # search clients children for canvas
+    canvas_name = "sun-awt-X11-XCanvasPeer".encode() # TODO: use recursion
+    for child in client.query_tree().children:  # tested for runelite only
+        for child1 in child.query_tree().children:
+            for child2 in child1.query_tree().children:
+                if ewmh.getWmName(child2) == canvas_name:
+                    return child2
+
+def get_window_frame(title):
+    return get_frame(get_window(title))
+
+class CS(Enum): # client state
     LOGGED_IN=0
     LOGIN=1
     WORLD_SWITCHER=2
     LOBBY=3
+
+class Client:
+    def __init__(self, window_title):
+        self.window_title=window_title
+        self.window = get_window(window_title)
+        self.canvas = get_canvas(self.window)
+        self.frame = get_window_frame(client_name)
+
+
