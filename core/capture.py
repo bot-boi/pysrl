a431805122f15f3bb712460a247cd7c23cb73288
bot-boi <bananajones532@gmail.com>
2020-08-11 05:16:22 -0400

    Changed to capture on demand

--- core/capture.py
+++ core/capture.py
@@ -1,7 +1,5 @@
-from threading import Thread, Lock
 from PIL import Image
 from Xlib import X, error as Xerror
-import time
 import numpy as np
 from ewmh import EWMH
 ewmh = EWMH()
@@ -54,7 +52,7 @@ class XObjPattern:
         """
         bools = []
         if self.id:
-            bools.append(self.id == ewmh.getWmPid(other))
+            bools.append(self.id == other.id)
         if self.name:
             othername = ewmh.getWmName(other)
             if type(othername) == bytes:  # sometimes wm names are bytes
@@ -72,19 +70,17 @@ SIMP_WINDOW = XObjPattern(name="Simplicity RSPS - The Biggest Pre-EOC Server 202
 SIMP_CANVAS = XObjPattern(name="sun-awt-X11-XPanelPeer")
 
 
-def findxobj(pattern: XObjPattern):
+def findxwindow(pattern: XObjPattern):
     """
     Finds an XWindow object recursively.
 
     Parameters
     -----------
-        pattern : XObjPattern
-            pattern describing the desired XWindow object
+        pattern - pattern describing the desired XWindow object
 
     Returns
     -------
-        matches : List[Xlib.display.Window]
-            list of xwindow objects that match the pattern
+        matches - list of xwindow objects that match the pattern
     """
     matches = []
 
@@ -101,71 +97,51 @@ def findxobj(pattern: XObjPattern):
     return matches
 
 
-# Capture class, represents constant capture of a window
-# init with Capture(ClientWindow("RuneLite"))
-class Capture:
-    def __init__(self, windowpattern, canvaspattern):
-        self.windowpattern = windowpattern
-        self.canvaspattern = canvaspattern
-        self.window = findxobj(windowpattern)[0]  # TODO: handle case no object and case multiple same object?
-        self.target = findxobj(canvaspattern)[0]  # where we grab img from
-        self.lock: Lock = Lock()    # mutex lock thing
-        self.thread: Thread = None  # the capture thread (init w/ self.start())
-        self.kill: bool = False     # used to kill thread capture thread
-        self.image: Image = None
+def get_frame(window):
+    # get window manager frame of window
+    while window.query_tree().parent.id != ewmh.root.id:
+        window = window.query_tree().parent
+    return window
 
-    # starts the capture thread
-    def start(self):
-        self.kill = False
-        self.thread = Thread(target=self.capture_thread)
-        self.thread.start()
 
-    # terminates the capture thread
-    def terminate(self):
-        self.kill = True
+def get_offset(window) -> (int, int):
+    # get x,y offset of a window relative to root
+    xoff = 0
+    yoff = 0
+    while window.query_tree().parent.id != ewmh.root.id:
+        g = window.get_geometry()
+        xoff += g.x
+        yoff += g.y
+        window = window.query_tree().parent
+    return (xoff, yoff)
 
-    # thread capture job
-    # TODO: it might make more sense to output arrays of pixels
-    # instead of Image objects
-    def capture_thread(self):
-        while True:
-            # t1 = time.time()
-            if self.kill:  # check if killed
-                return 0
 
-            # get the shape of the window and reload canvas if it fails
-            # canvas id changes on resize, other events cause this too ?
-            try:
-                g = self.target.get_geometry()
-            except Xerror.BadDrawable or Xerror.BadMatch:
-                print("bad drawable")
-                self.target = findxobj(self.canvaspattern)
-                g = self.target.get_geometry()
-            # get raw img data of window NOTE: these are python xlib calls
-            raw = self.target.get_image(0, 0, g.width, g.height, X.ZPixmap,
+class Capture:  #
+    def __init__(self, canvas_pattern: XObjPattern):
+        # get canvas and its frame and offset relative to root coords
+        self.canvas_pattern = canvas_pattern
+        self.canvas = findxwindow(self.canvas_pattern)[0]
+        self.frame = get_frame(self.canvas)
+        self.offset = get_offset(self.canvas)
+
+    def get_image(self):
+        # get the shape of the window and reload canvas if it fails
+        # canvas id changes on resize, other events cause this too ?
+        try:
+            g = self.canvas.get_geometry()
+        except Xerror.BadDrawable or Xerror.BadMatch:
+            print("bad drawable")
+            self.canvas = findxwindow(self.canvas_pattern)[0]
+            g = self.canvas.get_geometry()
+        raw = self.canvas.get_image(0, 0, g.width, g.height, X.ZPixmap,
+                                    0xffffffff)
+        if type(raw.data) is str:
+            # case: window covered
+            # TODO: handle minimized window?
+            ewmh.setActiveWindow(self.frame)
+            ewmh.display.flush()  # force targeted window to top
+            raw = self.canvas.get_image(0, 0, g.width, g.height, X.ZPixmap,
                                         0xffffffff)
-            # convert raw to opencv image
-            if type(raw.data) == str:
-                # this means window is minimized or covered (not rendering)
-                print('Window hidden, slowing capture to every 10 seconds',
-                      flush=True)
-                time.sleep(10)
-            else:
-                img = Image.frombytes("RGB", (g.width, g.height), raw.data,
-                                      "raw", "BGRX")
-                if img is None:
-                    print('img grab failed')
-                # output image
-                self.lock.acquire()
-                self.image = np.array(img.copy())
-                self.lock.release()
-                # t2 = time.time()
-                # print("capture thread ran in {}".format(t2-t1))
-
-    # returns latest capture
-    def get_image(self) -> np.ndarray:
-        self.lock.acquire()
-        arr = np.copy(self.image)
-        self.lock.release()
-        print(arr)
-        return arr
+        img = Image.frombytes("RGB", (g.width, g.height), raw.data,
+                              "raw", "BGRX")
+        return np.array(img)
